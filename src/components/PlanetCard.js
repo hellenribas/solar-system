05fa7002d0592f8633dac3271b21b9618ad8adf3
hellenribas <tchwtrabalho@gmail.com>
2022-02-18 16:46:48 -0300

    requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+};
+
+PlanetCard.defaultProps = {
+  planetName: 'Planeta',
+  planetImage: 'Image Inválida',
+};
+
+export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <section key={ name }>
+            <PlanetCard name={ name } image={ image } />
+          </section>
+        ))}
       </div>
     );
   }
