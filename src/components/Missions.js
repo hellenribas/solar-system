6963183a137e89fc24560a93a7dacf57733d5ce0
hellenribas <tchwtrabalho@gmail.com>
2022-02-18 17:01:34 -0300

    requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        ola mundo
+      </div>
+    );
+  }
+}
+
+export default Missions;
